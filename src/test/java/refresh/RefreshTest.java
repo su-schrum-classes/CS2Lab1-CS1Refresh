66b0282f13527011ce78bf10b2354b41cd3857fd
Jacob Schrum <schrum2@southwestern.edu>
2022-11-30 05:15:35 +0000

    Unit test starting point (no tests yet)

---
+++ src/test/java/refresh/RefreshTest.java
@@ -0,0 +1,114 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+import org.junit.jupiter.api.Test;
+
+class RefreshTest {
+
+    @Test
+    void testExercise1() {
+
+    }
+	
+    @Test
+    void testExercise2() {
+        
+    }
+
+    @Test
+    void testExercise3() {
+        
+    }
+
+    @Test
+    void testExercise4() {
+        
+    }
+
+    @Test
+    void testExercise5() {
+        
+    }
+
+    @Test
+    void testExercise6() {
+        
+    }
+
+    @Test
+    void testExercise7() {
+        
+    }
+
+    @Test
+    void testExercise8() {
+        
+    }
+
+    @Test
+    void testExercise9() {
+        
+    }
+
+    @Test
+    void testExercise10() {
+        
+    }
+
+    @Test
+    void testExercise11() {
+        
+    }
+
+    @Test
+    void testExercise12() {
+        
+    }
+
+    @Test
+    void testExercise13() {
+        
+    }
+
+    @Test
+    void testExercise14() {
+        
+    }
+
+    @Test
+    void testExercise15() {
+        
+    }
+
+    @Test
+    void testExercise16() {
+        
+    }
+
+    @Test
+    void testExercise17() {
+        
+    }
+
+    @Test
+    void testExercise18() {
+        
+    }
+
+    @Test
+    void testExercise19() {
+        
+    }
+
+    @Test
+    void testExercise20() {
+        
+    }
+
+    @Test
+    void testExercise21() {
+        
+    }
+
+}
