2f7f19ffebdcea1d94626054557eef1d773d141b
Jacob Schrum <schrum2@southwestern.edu>
2022-11-30 06:09:02 +0000

    Progress on tests

--- src/test/java/refresh/RefreshTest.java
+++ src/test/java/refresh/RefreshTest.java
@@ -39,37 +39,87 @@ class RefreshTest {
 	
     @Test
     void testExercise2() {
-        
+        Refresh.exercise2();
+        assertEquals("4.795831523312719"+System.lineSeparator(), byteOutputStream.toString());
     }
 
     @Test
     void testExercise3() {
-        
+        Refresh.exercise3();
+        assertEquals("1"+System.lineSeparator()+
+                     "2"+System.lineSeparator()+
+                     "3"+System.lineSeparator()+
+                     "4"+System.lineSeparator()+
+                     "5"+System.lineSeparator()+
+                     "6"+System.lineSeparator()+
+                     "7"+System.lineSeparator()+
+                     "8"+System.lineSeparator()+
+                     "9"+System.lineSeparator()+
+                     "10"+System.lineSeparator(), byteOutputStream.toString());
     }
 
     @Test
     void testExercise4() {
-        
+        Refresh.exercise4();
+        assertEquals("4560"+System.lineSeparator(), byteOutputStream.toString());
     }
 
     @Test
     void testExercise5() {
-        
+        Refresh.exercise5(1,2);
+        Refresh.exercise5(1000,200);
+        Refresh.exercise5(-100,50);
+        Refresh.exercise5(12,23);
+        Refresh.exercise5(-100.05,50.25);
+        Refresh.exercise5(12.001,23.5);
+        assertEquals("3.0"+System.lineSeparator()+
+                     "1200.0"+System.lineSeparator()+
+                     "-50.0"+System.lineSeparator()+
+                     "35.0"+System.lineSeparator()+
+                     "-49.8"+System.lineSeparator()+
+                     "35.501"+System.lineSeparator(), byteOutputStream.toString());
     }
 
     @Test
     void testExercise6() {
-        
+        assertEquals(3.0,Refresh.exercise6(1,2));
+        assertEquals(1200.0,Refresh.exercise6(1000,200));
+        assertEquals(-50.0,Refresh.exercise6(-100,50));
+        assertEquals(35.0,Refresh.exercise6(12,23));
+        assertEquals(-49.8,Refresh.exercise6(-100.05,50.25));
+        assertEquals(35.501,Refresh.exercise6(12.001,23.5));
     }
 
     @Test
     void testExercise7() {
+        Refresh.exercise7();
+        assertEquals("**********"+System.lineSeparator()+
+                     "**********"+System.lineSeparator()+
+                     "**********"+System.lineSeparator()+
+                     "**********"+System.lineSeparator()+
+                     "**********"+System.lineSeparator()+
+                     "**********"+System.lineSeparator()+
+                     "**********"+System.lineSeparator()+
+                     "**********"+System.lineSeparator()+
+                     "**********"+System.lineSeparator()+
+                     "**********"+System.lineSeparator(), byteOutputStream.toString());
         
     }
 
     @Test
     void testExercise8() {
-        
+        Refresh.exercise8(10,3);
+        Refresh.exercise8(2,5);
+        Refresh.exercise8(4,1);
+        assertEquals("**********"+System.lineSeparator()+
+                     "**********"+System.lineSeparator()+
+                     "**********"+System.lineSeparator()+
+                     "**"+System.lineSeparator()+
+                     "**"+System.lineSeparator()+
+                     "**"+System.lineSeparator()+
+                     "**"+System.lineSeparator()+
+                     "**"+System.lineSeparator()+
+                     "****"+System.lineSeparator(), byteOutputStream.toString());
     }
 
     @Test
